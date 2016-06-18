b1c1a8366ad208fb3314f815764bdf8effd47656
Walter Galan <mr2incredible@gmail.ccom>
2016-06-17 17:49:17 -0700

    Repackage set difference into one function.

--- vecOps.cpp
+++ vecOps.cpp
@@ -11,59 +11,68 @@
 
 using namespace std;
 
+vector<double> setDifference(vector<double> setA, vector<double> setB);
+
 int main () {
 	
  	// Define two sets of vectors
-	vector<double> setA = {1, 4, 5, 8, 11, 13, 16};
-	vector<double> setB = {2, 5, 11};
+  vector<double> setA = {1, 4, 5, 8, 11, 13, 16};
+  vector<double> setB = {2, 5, 11};
 	
  	// Vector set containing values in set A but not in B
  	vector<double> setAPrime;
  
-	 // Determine size of each vector set
-	int sizeA = setA.size();
-	int sizeB = setB.size();
-
- 	// Display size of each vector set
-	cout << "Set A contains " << sizeA << "values" << endl;
-	cout << "Set B contains " << sizeB << "values" << endl;
+  setAPrime = setDifference(setA, setB);
  
- 	int i = 0;
-	int j = 0;
- 
- 	// Determine which values in set A but not in set B
- 	while (i < sizeA && j < sizeB) {
-  		if (setA[i] < setB[j]) {
-   			setAPrime.push_back(setA[i]);
-   			i++;
-  		}
-  		else if (setA[i] > setB[j]) {
-   			j++;
-  		}
-  		else {
-   			i++;
-   			j++;
-  		}
- 	}
- 
-	// Append remaining values
- 	if (sizeA > sizeB) {
-  		while (i < sizeA) {
-   			setAPrime.push_back(setA[i]);
-   			i++;
-  		}
- 	}
- 	else {
-  		while (j < sizeB) {
-   			setAPrime.push_back(setB[j]);
-   			j++;
-  		}
- 	}
- 
- 	// Display contents of vector containing values in A but not in B
- 	for (i = 0; i < setAPrime.size(); i++) {
+  // Display contents of vector containing values in A but not in B
+ 	for (int i = 0; i < setAPrime.size(); i++) {
   		cout << "Contents of APrime: " << setAPrime[i] << endl;
  	}
  
  	return 0;
-}	
+}
+
+// Determine values in set A but not in set B
+vector<double> setDifference(vector<double> setA, vector<double> setB) {
+ 
+ // Determine size of each vector set
+	int sizeA = setA.size();
+	int sizeB = setB.size();
+ 
+ int i = 0;
+	int j = 0;
+ 
+ vector<double> setDiff;
+ 
+ // Determine which values in set A but not in set B
+ while (i < sizeA && j < sizeB) {
+  if (setA[i] < setB[j]) {
+   setDiff.push_back(setA[i]);
+   i++;
+  }
+  else if (setA[i] > setB[j]) {
+   j++;
+  }
+  else {
+   i++;
+   j++;
+  }
+ }
+ 
+	// Append remaining values
+ if (sizeA > sizeB) {
+  while (i < sizeA) {
+   setDiff.push_back(setA[i]);
+   i++;
+  }
+ }
+ else {
+  while (j < sizeB) {
+   setDiff.push_back(setB[j]);
+   j++;
+  }
+ }
+ 
+ return setDiff;
+}
+
