d9ee38c6099f940a4b57400aca5f6ac3834e135d
Walter Galan <mr2incredible@gmail.ccom>
2016-06-17 18:12:10 -0700

    Implemented intersection of two sets.

--- vecOps.cpp
+++ vecOps.cpp
@@ -12,28 +12,48 @@
 using namespace std;
 
 vector<double> setDifference(vector<double> setA, vector<double> setB);
+vector<double> setIntersection(vector<double> setA, vector<double> setB);
+
+// Next step is to implement set union
+vector<double> setUnion(vector<double> setA, vector<double> setB);
 
 int main () {
 	
- 	// Define two sets of vectors
-  vector<double> setA = {1, 4, 5, 8, 11, 13, 16};
-  vector<double> setB = {2, 5, 11};
+ // Define two sets of vectors
+ vector<double> setA = {1, 2, 4, 5, 8, 11, 13, 16};
+ vector<double> setB = {2, 5, 11};
 	
- 	// Vector set containing values in set A but not in B
- 	vector<double> setAPrime;
+ // Vector set containing values in set A but not in B
+ vector<double> setOfDifference;
  
-  setAPrime = setDifference(setA, setB);
+ // Vector set containing values in set A that are also in set B
+ vector<double> setOfIntersection;
  
-  // Display contents of vector containing values in A but not in B
- 	for (int i = 0; i < setAPrime.size(); i++) {
-  		cout << "Contents of APrime: " << setAPrime[i] << endl;
- 	}
+ setOfDifference = setDifference(setA, setB);
+ setOfIntersection = setIntersection(setA, setB);
  
- 	return 0;
+ cout << "\n***** Difference *****" << endl;
+ 
+ // Display contents of difference vector
+ for (int i = 0; i < setOfDifference.size(); i++) {
+   cout << "Values in Set A but not in Set B: " << setOfDifference[i] << endl;
+ }
+ 
+ cout << "\n***** Intersection *****" << endl;
+ 
+ // Display contents of intersection vector
+ for (int i = 0; i < setOfIntersection.size(); i++) {
+  cout << "Values in Set A that are also in Set B: " << setOfIntersection[i] << endl;
+ }
+ 
+ return 0;
 }
 
-// Determine values in set A but not in set B
+// Determines difference in values of two sets
 vector<double> setDifference(vector<double> setA, vector<double> setB) {
+ 
+ // Contains difference values
+ vector<double> setDiff;
  
  // Determine size of each vector set
 	int sizeA = setA.size();
@@ -41,8 +61,6 @@ vector<double> setDifference(vector<double> setA, vector<double> setB) {
  
  int i = 0;
 	int j = 0;
- 
- vector<double> setDiff;
  
  // Determine which values in set A but not in set B
  while (i < sizeA && j < sizeB) {
@@ -76,3 +94,32 @@ vector<double> setDifference(vector<double> setA, vector<double> setB) {
  return setDiff;
 }
 
+// Determines intersection of two sets
+vector<double> setIntersection(vector<double> setA, vector<double> setB) {
+ 
+ vector<double> setInter;
+ 
+ // Determine size of each vector set
+	int sizeA = setA.size();
+	int sizeB = setB.size();
+ 
+ int i = 0;
+	int j = 0;
+ 
+ // Determine which values in set A and also in set B
+ while (i < sizeA && j < sizeB) {
+  if (setA[i] < setB[j]) {
+   i++;
+  }
+  else if (setA[i] > setB[j]) {
+   j++;
+  }
+  else {
+   setInter.push_back(setA[i]);
+   i++;
+   j++;
+  }
+ }
+
+ return setInter;
+}
